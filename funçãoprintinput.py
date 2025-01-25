c130720c06ade44d49c27ce0cd6b84a03773072f
angelusaloise <angelus_aloise@hotmail.com>
2025-01-25 12:59:11 +0000

    teste

---
+++ funçãoprintinput.py
@@ -0,0 +1,8 @@
+import math
+
+#pedir ao utilizador um número inteiro e devolver sua raiz quadrada
+numero = int(input("introduza um numero inteiro!"))
+#numero=int(numero)
+raizquadrada = math.sqrt(numero)
+
+print("a raiz quadrada do numero é:",numero,"igual a", round(raizquadrada,2))
